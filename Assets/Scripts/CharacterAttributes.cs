cb1579f730a0cdea15774bc42237ec8bc26a7271
Pablo Reszczynski <pablore@me.com>
2015-01-26 21:48:55 -0300

    se

---
+++ Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAttributes : MonoBehaviour {
+
+	public int Strength = 0;
+	public int Intelligence = 0;
+	public int Agility = 0;
+
+	int base_hp;
+	int base_mp;
+	int base_armor;
+
+	void Start(){
+		base_hp = 100 + Strength * 10;
+		base_mp = 100 + Intelligence * 10;
+		base_armor = Agility / 3;
+	}
+}
